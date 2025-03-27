0fdb474bb0839735c95f2918cfe77399c0d5a325
ahmadid15 <idrisahmadd15@gmail.com>
2025-03-27 13:28:57 +0100

    added regtest

--- submission/03.sh
+++ submission/03.sh
@@ -1,7 +1,7 @@
 # Created a SegWit address.
-segwit_address=$(bitcoin-cli getnewaddress "" bech32)
+segwit_address=$(bitcoin-cli -regtest getnewaddress "" bech32)
 
 # Add funds to the address.
-transaction_id=$(bitcoin-cli sendtoaddress "$segwit_address" 0.01)
+transaction_id=$(bitcoin-cli -regtest sendtoaddress "$segwit_address" 0.01)
 # Return only the Address
 cat $segwit_address
