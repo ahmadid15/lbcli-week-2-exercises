3aa66254a3f8c8da82ebb24af3151f70592d8d2d
ahmadid15 <idrisahmadd15@gmail.com>
2025-03-27 17:30:00 +0100

    generatetoaddress used

--- submission/03.sh
+++ submission/03.sh
@@ -4,4 +4,4 @@ segwit_address=$(bitcoin-cli -regtest getnewaddress "" "bech32")
 # Add funds to the address.
 transaction_id=$(bitcoin-cli -regtest generatetoaddress 101 "$segwit_address")
 # Return only the Address
-cat $segwit_address
+#cat $segwit_address
